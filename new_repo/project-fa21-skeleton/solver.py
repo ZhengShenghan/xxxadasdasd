662414aa97fa0de28750f3ee5c8132a68e167093
Jasper Emhoff <jlegoal@berkeley.edu>
2021-12-07 15:26:01 -0800

    Refactored solver.py

--- new_repo/project-fa21-skeleton/solver.py
+++ new_repo/project-fa21-skeleton/solver.py
@@ -3,11 +3,6 @@ import math
 import os
 import random
 import dynamic
-from threading import Thread
-from multiprocessing.pool import ThreadPool
-import FuncSets
-import FuncSets_BFS
-import Task
 import sys
 import FuncSets_simulated_annealing
 sys.setrecursionlimit(20000)
@@ -80,8 +75,8 @@ def naive_simulated_annealing(tasks, temp):
     return f_sequence, profit
 
 
-def bench_mark(tasks, eval):
-    profit_margin = EVAL_FUNCTIONS[eval]
+def bench_mark(tasks):
+    profit_margin = EVAL_FUNCTIONS['adv_profit_ratio']
     deadline = EVAL_FUNCTIONS['deadline_profit']
     sequence = []
     slip = 10
@@ -122,26 +117,9 @@ def bench_mark(tasks, eval):
                 # Add expected profit from igloo
                 profit += choice.get_late_benefit(overtime)
 
-    # review_and_revise(sequence, profit, tasks)
     f_sequence = [task.get_task_id() for task in sequence]
     assert time <= 1440, f'Tasks time {time} exceed the limit of 1440'
     return f_sequence, profit
-
-
-def review_and_revise(curr_sequence, curr_profit, rem_tasks):
-    time = 0
-    for i in range(len(curr_sequence)):
-        curr_task = curr_sequence[i]
-        curr_task_overtime = time + curr_task.get_duration() - curr_task.get_deadline()
-        curr_task_profit = curr_task.get_late_benefit(curr_task_overtime)
-        for t in rem_tasks:
-            t_overtime = time + t.get_duration() - t.get_deadline()
-            t_profit = t.get_late_benefit(t_overtime)
-            if curr_task_profit < t_profit:
-                copy_seq = curr_sequence.copy()
-                copy_seq[i] = t
-                if curr_profit < determine_profit(copy_seq):
-                    curr_sequence[i] = t
 
 
 def swap(sequence, curr_task):
@@ -165,15 +143,29 @@ def determine_profit(sequence):
         profit += task.get_late_benefit(overtime)
     return profit
 
-# def check_outputs():
-#     for test_type in os.listdir('outputs/'):
-#         if test_type[0] != '.':
-#             for out_path in os.listdir('outputs/' + test_type):
-#                 if out_path[0] != '.':
-#                     output_path = 'outputs/' + test_type + '/' + out_path[:-3] + '.out'
-#                     with open(out_path, encoding="utf-8-sig") as out_file:
-#                         while out_file
-#
+
+def run_mult_alg(tasks):
+    tasks1 = tasks.copy()
+    tasks2 = tasks.copy()
+    tasks3 = tasks.copy()
+    out1 = 0, 0
+    out2 = 0, 0
+    out3 = 0, 0
+    branch_bound = FuncSets_simulated_annealing.branch_and_bound(tasks2)
+
+    # Simply greedy
+    out1 = bench_mark(tasks1)
+
+    # Branch and bound
+    out2 = branch_bound.return_sequence(), branch_bound.result()
+
+    # Simulated Annealing
+    out3 = naive_simulated_annealing(tasks3, 2000)
+
+    sequence, profit = max([out1, out2, out3], key=lambda x: x[1])
+    write_output_file(output_path, sequence)
+    return profit
+
 
 # Here's an example of how to run your solver.
 if __name__ == '__main__':
@@ -182,14 +174,6 @@ if __name__ == '__main__':
     bp2 = 0
     bp3 = 0
     net = 0
-    # task0 = Task.Task(0, 936, 432, 10)
-    # task1 = Task.Task(1, 1440, 576, 20)
-    # task2 = Task.Task(2, 1152, 216, 5)
-    # task3 = Task.Task(3, 432, 288, 30)
-    # task4 = Task.Task(4, 576, 360, 15)
-    # tasks = [task0, task1, task2, task3, task4]
-    # print(dynamic.dp_solver(tasks))
-    pool = ThreadPool(processes=16)
     for test_type in os.listdir('inputs/'):
         if test_type[0] != '.':
             for input_path in os.listdir('inputs/' + test_type):
@@ -198,76 +182,12 @@ if __name__ == '__main__':
                     input_path = 'inputs/' + test_type + '/' + input_path
                     tasks = read_input_file(input_path)
 
-                    # async_result = pool.apply_async(dynamic.dp_solver, (tasks,))  # tuple of args for foo
-                    #
-                    # # do some other stuff in the main process
-                    # return_val = async_result.get()  # get the return value from your function.
-                    # net += return_val[0]
-                    # write_output_file(output_path, return_val[1])
-
-
-                    # tasks1 = tasks.copy()
-                    # tasks2 = tasks.copy()
-                    # tasks3 = tasks.copy()
-                    # out1 = 0, 0
-                    # out2 = 0, 0
-                    # out3 = 0, 0
-                    # out4 = 0, 0
                     p, s = dynamic.dp_solver(tasks)
                     net += p
                     write_output_file(output_path, s)
-                    # out1 = bench_mark(tasks1, 'adv_profit_ratio')
-                    # branch_bound = FuncSets_simulated_annealing.branch_and_bound(tasks)
-                    # out2 = branch_bound.return_sequence(), branch_bound.result()
-                    # out3 = naive_simulated_annealing(tasks3, 2000)
 
+                    # Uncomment to run the three algorithm stack
+                    # net += run_mult_alg(tasks)
 
-                    # sequence, profit = max([out1, out2, out3], key=lambda x: x[1])
-                    # net += profit
-                    # write_output_file(output_path, sequence)
-                    #
-                    # bp1 += out1[1]
-                    # bp2 += out2[1]
-                    # bp3 += out3[1]
                     count += 1
     print(f'Net: {net/count}')
-    # print(f'Benchmark1 Avg: {bp1/count}\nSimulated Annealing Avg: {bp2/count}\nNaive Simulated Annealing Avg: {bp3/count}\nNet: {net/count}')
-
-# if __name__ == '__main__':
-#     total = 0
-#     count = 0
-#     for test_type in os.listdir('inputs/'):
-#         if test_type[0] != '.':
-#             for input_path in os.listdir('inputs/' + test_type):
-#                 if input_path[0] != '.':
-#                     output_path = 'outputs/' + test_type + '/' + input_path[:-3] + '.out'
-#                     input_path = 'inputs/' + test_type + '/' + input_path
-#                     tasks = read_input_file(input_path)
-#                     branch_and_bound = FuncSets_simulated_annealing.branch_and_bound(tasks)
-#                     total += branch_and_bound.result()
-#                     count += 1
-#                     # print(branch_and_bound.result())
-#                     # if count % 200:
-#                     #     print(total/count)
-#                     # print('\n')
-#     print(f'Average profit: {total/count}')
-# =======
-#     sum = 0
-#     num = 0
-#     for test_type in os.listdir('inputs/'):
-#             if test_type[0] != '.':
-#                 for input_path in os.listdir('inputs/' + test_type):
-#                     if input_path[0] != '.':
-#                         output_path = 'outputs/' + test_type + '/' + input_path[:-3] + '.out'
-#                         input_path = 'inputs/' + test_type + '/' + input_path
-#                         tasks = read_input_file(input_path)
-#                         branch_and_bound = FuncSets_simulated_annealing.branch_and_bound(tasks)
-#                         t = branch_and_bound.result()
-#                         sum += t
-#                         branch_and_bound.show()
-#                         print(t)
-#                         num += 1
-#                         print('\n')
-#     print("---")
-#     print(sum/num)
-# >>>>>>> a3c8ca6ac7e483f7163815d70b843dde17b8c77d
